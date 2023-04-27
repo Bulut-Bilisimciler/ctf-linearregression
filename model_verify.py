cc963f2edf43a9f7313f6c3f79ca89384f9ac8f4
Yamac Tan <63727076+YamacTan@users.noreply.github.com>
2023-04-27 18:07:55 +0300

    Update model_verify.py

--- model_verify.py
+++ model_verify.py
@@ -5,19 +5,16 @@ import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score
 import json
 import requests
-import subprocess    
+import subprocess
+
 
 def verifyScore():
 
-# Linux Home directory ayarı
-    home_dir = os.path.expanduser("~")
-    accuracy = 0 
-
     # Search for Pickle extensioned files in the home directory
-    for file_name in os.listdir(home_dir):
+    for file_name in os.listdir("/home"):
         if file_name.endswith('.pkl'):
             # Found the model file
-            model_file_path = os.path.join(home_dir, file_name)
+            model_file_path = os.path.join("/home", file_name)
             # print(f"Found model file: {model_file_path}")
 
             # Load the model
@@ -25,7 +22,8 @@ def verifyScore():
                 model = pickle.load(f)
 
             # Load the test dataset
-            test_df = pd.read_csv('/home/maintest.csv')
+            test_df = pd.read_csv('maintest.csv')
+            test_df.dropna(inplace=True)
 
             # Extract the target values
             y_true = test_df['target'].values
@@ -37,28 +35,25 @@ def verifyScore():
             y_pred = model.predict(X_test)
 
             # Calculate the performance metrics
-            mse = mean_squared_error(y_true, y_pred)
-            mae = mean_absolute_error(y_true, y_pred)
+            # mse = mean_squared_error(y_true, y_pred)
+            # mae = mean_absolute_error(y_true, y_pred)
             r2 = r2_score(y_true, y_pred)
-            accuracy = accuracy_score(y_true, y_pred.round())
+            # accuracy = np.sqrt(mean_squared_error(y_true, y_pred))
 
             # Print the results
-            print(f"Mean squared error: {mse}")
-            print(f"Mean absolute error: {mae}")
+            # print(f"Mean squared error: {mse}")
+            # print(f"Mean absolute error: {mae}")
             print(f"R-squared: {r2}")
-            print(f"Accuracy: {accuracy}")
+            # print(f"Accuracy: {accuracy}")
 
             # Log message to inform user that calculations are complete
             print("Model evaluation complete.")
 
-
-    if (accuracy>0.3):
+    if (r2>0.5):
         subprocess.call(['echo', '0'])
         return 0
     else:
         subprocess.call(['echo', '1'])
         return 1
-    
+
 result = verifyScore()
-
-
